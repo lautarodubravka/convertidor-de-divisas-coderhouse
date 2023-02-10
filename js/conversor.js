9de6f28f41ce9ffc9b84f99cc0d227597ddb372b
Lautaro Dubravka <lautarodubravka7@gmail.com>
2023-02-10 00:11:30 -0300

    update 4.4

--- js/conversor.js
+++ js/conversor.js
@@ -1,12 +1,11 @@
 const API_KEY = "YA430D84G5AYNSJV"; /*La clave API iría en el backend para mayor seguridad*/
 const exchangeRates = {};
-
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-    let message = document.getElementById("important-message");
+document.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    const message = document.getElementById("important-message");
     message.style.display = "block";
     let opacity = 0;
-    let timer = setInterval(function() {
+    const timer = setInterval(() => {
       opacity += 0.05;
       message.style.opacity = opacity;
       if (opacity >= 1) {
@@ -21,40 +20,36 @@ function convertCurrency(amount, exchangeRate, fromCurrency, toCurrency) {
   }
   if (exchangeRate) {
     return (amount * exchangeRate).toFixed(0);
-  } else {
-    return "Tasa de cambio no disponible";
   }
+  return "Tasa de cambio no disponible";
 }
 async function updateExchangeRates(fromCurrency, toCurrency) {
   document.getElementById("loading-spinner").style.display = "block";
   try {
-      const response = await fetch(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurrency}&to_currency=${toCurrency}&apikey=${API_KEY}`);
-      const data = await response.json();
-      if(data["Realtime Currency Exchange Rate"] && data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]){
-          exchangeRates[toCurrency] = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
-      } else {
-          exchangeRates[toCurrency] = null;
-      }
+    const response = await fetch(`https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurrency}&to_currency=${toCurrency}&apikey=${API_KEY}`);
+    const data = await response.json();
+    const exchangeRate = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+    exchangeRates[toCurrency] = exchangeRate || null;
   } catch (error) {
-      console.log(error);
+    console.error(error);
+  } finally {
+    document.getElementById("loading-spinner").style.display = "none";
   }
-  document.getElementById("loading-spinner").style.display = "none";
 }
-
 const form = document.getElementById("conversionForm");
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
   const amount = document.getElementById("amount").value;
-  const fromCurrency = document.getElementById("fromCurrency").options[document.getElementById("fromCurrency").selectedIndex].value;
-  const toCurrency = document.getElementById("toCurrency").options[document.getElementById("toCurrency").selectedIndex].value;
+  const fromCurrency = document.getElementById("fromCurrency").value;
+  const toCurrency = document.getElementById("toCurrency").value;
   const result = document.getElementById("result");
   try {
-      await updateExchangeRates(fromCurrency, toCurrency);
-      const convertedAmount = convertCurrency(amount, exchangeRates[toCurrency], fromCurrency, toCurrency);
-      result.innerHTML = `${amount} ${fromCurrency} equivale a ${convertedAmount} ${toCurrency}`;
-      addToConversionHistory(convertedAmount, fromCurrency, toCurrency, new Date(), amount);
-      displayConversionHistory();
+    await updateExchangeRates(fromCurrency, toCurrency);
+    const convertedAmount = convertCurrency(amount, exchangeRates[toCurrency], fromCurrency, toCurrency);
+    result.innerHTML = `${amount} ${fromCurrency} equivale a ${convertedAmount} ${toCurrency}`;
+    addToConversionHistory(convertedAmount, fromCurrency, toCurrency, new Date(), amount);
+    displayConversionHistory();
   } catch (error) {
-      console.log(error);
+    console.error(error);
   }
 });
--- js/historial.js
+++ js/historial.js
@@ -1,6 +1,5 @@
 /*TABLA DE HISTORIAL DE BUSQUEDAS REALIZADAS POR EL USUARIO*/
 let conversionHistory = [];
-
 function addToConversionHistory(convertedAmount, fromCurrency, toCurrency, date, amount) {
   conversionHistory.push({
     convertedAmount,
@@ -10,33 +9,33 @@ function addToConversionHistory(convertedAmount, fromCurrency, toCurrency, date,
     amount
   });
 }
-
 function displayConversionHistory(history = conversionHistory) {
   const historyTable = document.getElementById("conversion-history");
   historyTable.innerHTML = "";
-  for (let i = 0; i < history.length; i++) {
-    const conversion = history[i];
+  for (const conversion of history) {
     const row = document.createElement("tr");
-    row.innerHTML = `<td>${conversion.fromCurrency}</td> <td>${conversion.toCurrency}</td> <td>${conversion.amount}</td> <td>${conversion.convertedAmount}</td> <td>${conversion.date}</td>`;
+    row.innerHTML = `
+      <td>${conversion.fromCurrency}</td>
+      <td>${conversion.toCurrency}</td>
+      <td>${conversion.amount}</td>
+      <td>${conversion.convertedAmount}</td>
+      <td>${conversion.date}</td>
+    `;
     historyTable.appendChild(row);
   }
 }
   /*Campo de búsqueda*/
-
   const searchField = document.getElementById("searchInput");
-  searchField.addEventListener("keyup", () => {
-  const searchValue = searchField.value.toUpperCase();
-  const filteredHistory = conversionHistory.filter(conversion => conversion.fromCurrency.startsWith(searchValue) || conversion.toCurrency.startsWith(searchValue));
-  displayConversionHistory(filteredHistory);
+  searchField.addEventListener("input", () => {
+    const searchValue = searchField.value.toUpperCase();
+    const filteredHistory = conversionHistory.filter(conversion => conversion.fromCurrency.startsWith(searchValue) || conversion.toCurrency.startsWith(searchValue));
+    displayConversionHistory(filteredHistory);
   });
-
 function searchConversionHistory() {
   // Obtener el valor ingresado en el campo de búsqueda
   const searchValue = document.getElementById("searchInput").value;
-
   // Crear un nuevo arreglo para almacenar los resultados de búsqueda
   const searchResults = [];
-
   // Recorrer el historial de conversiones
   for (let i = 0; i < conversionHistory.length; i++) {
     const conversion = conversionHistory[i];
@@ -49,5 +48,6 @@ function searchConversionHistory() {
   // Pasar el arreglo de resultados de búsqueda a la función displayConversionHistory para mostrar solo las conversiones coincidentes
   displayConversionHistory(searchResults);
 }
-
-/*Los comentarios sé que pueden ser muchos pero me sirven a mí mismo para recordar y repasar el propósito del código*/
+/*Los comentarios sé que pueden ser muchos pero 
+me sirven a mí mismo para recordar y
+ repasar el propósito del código*/
